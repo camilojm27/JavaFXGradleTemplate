a55d332950057ed57f006a0143e57f62519b4bee
Camilo José Mezú Mina <camilo272001@gmail.com>
2020-09-30 22:10:40 -0500

    Ahora se compila el jar con el javaFX, el binario aparece en build/libs

---
+++ README.md
@@ -0,0 +1,3 @@
+# This is a javaFX with gradle template and itelliJ (must work with othe IDE too)
+ have
+if your are under console use the comand ./gradlew run **Only for Unix** otherwise you need gradle installed on your pc or with the ide
---
+++ src/main/java/com/gestor/recibos/HelloFX.java
@@ -0,0 +1,26 @@
+package com.gestor.recibos;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloFX extends Application {
+
+  public static void main(String[] args) { launch(args); }
+
+  @Override
+  public void start(Stage primaryStage) {
+    Parent root = null;
+    try {
+      root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    Scene scene = new Scene(root);
+    primaryStage.setScene(scene);
+    primaryStage.show();
+  }
+}
